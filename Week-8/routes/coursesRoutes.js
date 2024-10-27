8c382a38b42ec2c62b2bef91e9244e38641183f3
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-10-27 23:04:21 +0530

    Implemented the purchase and preview routes

--- Week-8/routes/coursesRoutes.js
+++ Week-8/routes/coursesRoutes.js
@@ -1,10 +1,30 @@
 const {Router}=require("express")
 const courseRoutes=Router()
-courseRoutes.post("/purchase",function (req,res) {
-    res.send("Course Purchase Routes")
+const {purchaseModel,courseModel}=require("../database/db.js")
+
+courseRoutes.post("/purchase", async function (req,res) {
+    const userId=req.userId
+    const courseID=req.body.courseID
+
+ try {
+    await courseModel.create({
+        userId,
+        courseID
+    })
+    res.status(200).json({message:"course purchases",staus:"purchased"})
+ } catch (error) {
+    res.json({message:"Someting Went Wrong",error:error})
+ }
+
 })
-courseRoutes.get("/preview",function (req,res) {
-    res.send("Courses Preview Routes")
+courseRoutes.get("/preview",async function (req,res) {
+
+try{
+    const allCourses=await courseModel.find({})
+    res.status(200).json({message:"All Courses List", courses:allCourses})
+}catch(error){
+res.status(200).json({message:"Something went wrong", error:error})
+} 
 })
 
 module.exports={
