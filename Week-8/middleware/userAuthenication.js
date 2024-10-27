7eff7d3578b1e7b3fc745c78a99a3da7a6d683f2
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-10-28 00:35:50 +0530

    Added course purchases  endpoint for users

--- Week-8/middleware/userAuthenication.js
+++ Week-8/middleware/userAuthenication.js
@@ -1,8 +1,8 @@
 const jwt=require("jsonwebtoken")
-const {JWT_SECRET_USER}=require("../config")
+const {JWT_SECRET_USER}=require("../config.js")
 
 function userMiddleware(req,res,next) {
-     const token=req.header.token
+     const token=req.headers.token
      const verification=jwt.verify(token,JWT_SECRET_USER)
      if(verification){
         req.userId=verification.idToken,
