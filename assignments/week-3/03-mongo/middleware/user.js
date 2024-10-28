a2d0f40b9b4c1bf06642f1ca5a0e57d6ffa46612
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-10-28 23:48:27 +0530

    Added middleware for user endpoint

--- assignments/week-3/03-mongo/middleware/user.js
+++ assignments/week-3/03-mongo/middleware/user.js
@@ -1,6 +1,22 @@
+const {User} =require("../db/index")
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const username = req.headers.username
+    const password = req.headers.password
+    const userValidation = User.findOne({
+        username: username,
+        password: password
+    }).then(function (user) {
+        if (user) {
+            next()
+        }
+        else {
+            res.status(403).json({
+                message: "Admin does not exist"
+            })
+        }
+    })
 }
 
 module.exports = userMiddleware;
