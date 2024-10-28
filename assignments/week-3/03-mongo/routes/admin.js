78102633954575b4e2e15170b951b6fa77927b69
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-10-28 23:44:35 +0530

    Added Signup feature

--- assignments/week-3/03-mongo/routes/admin.js
+++ assignments/week-3/03-mongo/routes/admin.js
@@ -1,10 +1,20 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup', async(req, res) => {
+   const username=req.body.username
+   const password=req.body.password
+   await Admin.create({
+    username:username,
+    password:password
+   }
+)
+res.status(404).json({
+    msg:"Admin Signup Successfully"
+})
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
