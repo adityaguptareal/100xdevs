2825c478eb0c93a1372123608659a4607fe76411
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-10-28 23:47:25 +0530

    Added Middleware for the admin endpoint

--- assignments/week-3/03-mongo/middleware/admin.js
+++ assignments/week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,23 @@
 // Middleware for handling auth
+const { Admin } = require("../db/index")
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const username = req.headers.username
+    const password = req.headers.password
+    const userValidation = Admin.findOne({
+        username: username,
+        password: password
+    }).then(function (user) {
+        if (user) {
+            next()
+        }
+        else {
+            res.status(403).json({
+                message: "User does not exist"
+            })
+        }
+    })
 }
 
 module.exports = adminMiddleware;
