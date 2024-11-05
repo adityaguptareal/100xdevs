1f08af9d365ccd33e80568f868d2dc26e2b635fb
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-05 22:26:54 +0530

    updated the app

--- week-9/week-9.2/src/App.jsx
+++ week-9/week-9.2/src/App.jsx
@@ -1,26 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  
+  const [counterVisible, setCounterVisible] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounterVisible((c) => !c);
+    }, 5000);
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-<Counter/>
+      <div>Hi</div>
+      {counterVisible && <Counter></Counter>}
+      <div>Hello</div>
     </>
-  )
-  
+  );
 }
 
 function Counter() {
-  const [count, setCount] = useState(5)
+  const [count, setCount] = useState(0);
+  console.log("Counter");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-    <h1>{count}</h1>
-    <button onClick={()=>{setCount(count+1)}}>Counter</button>
-    <button onClick={()=>{setCount(count-1)}}>Counter Decrease</button>
+      <h1>{count}</h1>
+      <button onClick={() => { setCount(count + 1); }}>Counter</button>
+      <button onClick={() => { setCount(count - 1); }}>Counter Decrease</button>
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
