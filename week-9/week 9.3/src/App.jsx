6c0a07ac73f4c13344bce0d30ff72ff18a5a934f
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-09 13:18:58 +0530

    learning about conditional rendering

--- week-9/week 9.3/src/App.jsx
+++ week-9/week 9.3/src/App.jsx
@@ -6,29 +6,45 @@ import './App.css';
 function App() {
   return (
     <>
-      <PostComponent />
+      <div style={{ display: 'flex', gap: 10, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '99vh', width: '99vw', backgroundColor: 'gray', }}>
+        <PostComponent name={"Aditya"} followerCount={30} time={'2d'} image={'https://i0.wp.com/adityaguptareal.com/wp-content/uploads/2024/03/Aditya-kumar-Gupta-adityaguptareal-4.png?resize=300%2C300&ssl=1'} description={"Hi this is Aditya kumar Gupta a professional fre"} />
+        <PostComponent name={"Aditya"} followerCount={30} time={'2d'} image={'https://i0.wp.com/adityaguptareal.com/wp-content/uploads/2024/03/Aditya-kumar-Gupta-adityaguptareal-4.png?resize=300%2C300&ssl=1'} description={"Hi this is Aditya kumar Gupta a professional fre"} />
+        <PostComponent name={"Aditya"} followerCount={30} time={'2d'} image={'https://i0.wp.com/adityaguptareal.com/wp-content/uploads/2024/03/Aditya-kumar-Gupta-adityaguptareal-4.png?resize=300%2C300&ssl=1'} description={"Hi this is Aditya kumar Gupta a professional fre"} />
+        <PostComponent name={"Aditya"} followerCount={30} time={'2d'} image={'https://i0.wp.com/adityaguptareal.com/wp-content/uploads/2024/03/Aditya-kumar-Gupta-adityaguptareal-4.png?resize=300%2C300&ssl=1'} description={"Hi this is Aditya kumar Gupta a professional fre"} />
+      </div>
+
     </>
   );
 }
+const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, padding: 20 }
 
-function PostComponent() {
+function PostComponent({ name, followerCount, time, image, description }) {
   return (
     <>
-      <div style={{}}>
-        <img src="https://www.ifoto.ai/blog/wp-content/uploads/ai-photo-enhancer-1.jpeg" alt="" style={{ width: 500, borderRadius: 20 }} />
+      <div style={style}>
+        <div style={{ display: 'flex' }}>
+          <img src={image} alt="" style={{ width: 30, height: 30, borderRadius: 20 }} />
+          <div style={{ fontSize: 10, marginLeft: 10 }}>
+            <b>
+              {name}
+            </b>
+            <div>{followerCount} Followers</div>
+            
+            {time && <div style={{ display: 'flex' }}>
+
+              <div>{time}</div>
+              <img src="https://static.vecteezy.com/system/resources/thumbnails/019/053/744/small_2x/world-globe-earth-map-png.png" alt="" style={{ width: 5, height: 5 }} />
+            </div>}
+
+          </div>
+        </div>
+        <div style={{ fontSize: 12 }}>
+          {description}
+        </div>
       </div>
     </>
   );
 }
 
-function ReviewComponent(){
-  return(
-    <>
-    <div style={{backgroundColor:"red", width:200, height:200}}>
-      <img src="" alt="" />
-      <div>Desceription</div>
-    </div>
-    </>
-  )
-}
+
 export default App;
