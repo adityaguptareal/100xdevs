69fc49e608e1a1cf0fd0592703f105a5884db1de
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-09 16:14:49 +0530

    Notification bell counter

--- week-9/week 9.3/src/App.jsx
+++ week-9/week 9.3/src/App.jsx
@@ -1,26 +1,22 @@
 import { useState } from "react";
-import { PostComponent } from "./PostComponent";
-function App() {
-  const [posts, setposts] = useState([])
-  const postRendering = posts.map((post, index) => (
-    <PostComponent name={post.name} key={index} subtitle={post.subtitle} time={post.time} description={post.description} image={post.image}></PostComponent>
-  ))
-  function addPost() {
 
-    setposts([...posts, {
-      name: "harkirat",
-      subtitle: "10000 followers",
-      time: "2m ago",
-      image: "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
-      description: "What to know how to win big? Check out how these folks won $6000 in bounties."
-    }])
-  }
+function App() {
+  const [count, setCount] = useState([0])
   return (
     <>
-      <button onClick={addPost}>Add Post</button>
-      <div style={{ backgroundColor: "gray", height: '100vh', width: "100vw", display: "flex", flexDirection:"column", justifyContent: "center", alignItems: 'center' }}>
-        {postRendering}
+      <div style={{ position: "relative", width: '100px', height: "100px" }}>
+        <img src="https://cdn-icons-png.flaticon.com/512/455/455665.png" style={{ height: 100, width: 100, position: "releative", left: 0 }} alt="" />
+        <div style={{
+          borderRadius: 500, display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "red", height: 30, width: 30, position: "absolute",
+          top: 0,
+          right: -10
+        }}>{count}</div>
       </div>
+
+      <div style={{ marginLeft: 200 }}>
+        <button onClick={() => (setCount(parseInt(count) + 1))}>Counter</button>
+      </div>
+
     </>
   )
 }
