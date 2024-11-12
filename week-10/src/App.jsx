c520d946877b54d3b57cdd7a4783bfcc0d446420
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-12 19:29:09 +0530

    useNavigate hook in react

--- week-10/src/App.jsx
+++ week-10/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useNavigate, redirect } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -6,15 +6,16 @@ function App() {
   return (
     <>
       <BrowserRouter>
-    <div>
-      <Link to="/">Allen |</Link> 
-      <Link to="/Class11">Class 11 |</Link>
-      <Link to="/Class12">Class 12 |</Link>
-    </div>
+        <div>
+          <Link to="/">Allen |</Link>
+          <Link to="/Class11">Class 11 |</Link>
+          <Link to="/Class12">Class 12 |</Link>
+
+        </div>
         <Routes>
-          <Route path='/' element={<Allen/>} />
-          <Route path='/Class11' element={<Class11/>} />
-          <Route path='/Class12' element={<Class12/>} />
+          <Route path='/' element={<Allen />} />
+          <Route path='/Class11' element={<Class11 />} />
+          <Route path='/Class12' element={<Class12 />} />
         </Routes>
       </BrowserRouter>
 
@@ -24,16 +25,21 @@ function App() {
   )
 
   function Allen() {
+    const naigate=useNavigate()
+    function redirectUser(){
+      naigate("/Clsss12")
+    }
     return (
       <>
+      <button onClick={redirectUser}>Redirect Button</button>
         <div>Welcome to the allen home page</div>
       </>
 
-    )
-  }
+)
+}
 
-  function Class11() {
-    return (
+function Class11() {
+  return (
       <>
         <div>This is class 11th Program</div>
       </>
@@ -46,6 +52,8 @@ function App() {
       </>
     )
   }
+  
+  
 }
-
-export default App
+  export default App
+  
