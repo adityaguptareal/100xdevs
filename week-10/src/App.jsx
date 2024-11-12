abf4970ee9d0bc2a594d20fb54646b1d7098c0c6
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-12 21:44:35 +0530

    Layout in react js

--- week-10/src/App.jsx
+++ week-10/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link, useNavigate, redirect } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -6,16 +6,12 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <div>
-          <Link to="/">Allen |</Link>
-          <Link to="/Class11">Class 11 |</Link>
-          <Link to="/Class12">Class 12 |</Link>
-
-        </div>
         <Routes>
-          <Route path='/' element={<Allen />} />
-          <Route path='/Class11' element={<Class11 />} />
-          <Route path='/Class12' element={<Class12 />} />
+          <Route path='/' element={<Layout />} >
+            <Route index element={<Allen />} />
+            <Route path='/Class11' element={<Class11 />} />
+            <Route path='/Class12' element={<Class12 />} />
+          </Route>
         </Routes>
       </BrowserRouter>
 
@@ -24,22 +20,57 @@ function App() {
 
   )
 
+  function Layout() {
+    return (
+      <>
+        <div>
+
+          <Header />
+        </div>
+        <div>
+          <Outlet />
+        </div>
+        <div>
+
+          <Footer />
+        </div>
+      </>
+
+    )
+  }
+  function Footer() {
+    return (
+      <div style={{ width: "100vw", backgroundColor: "red" }}>This is footer</div>
+    )
+  }
+  function Header() {
+    return (
+
+      <div style={{ width: "100vw", backgroundColor: 'blue', color: "white" }}>
+        <Link to="/">Allen |</Link>
+        <Link to="/Class11">Class 11 |</Link>
+        <Link to="/Class12">Class 12 |</Link>
+
+      </div>
+    )
+  }
+
   function Allen() {
-    const naigate=useNavigate()
-    function redirectUser(){
+    const naigate = useNavigate()
+    function redirectUser() {
       naigate("/Clsss12")
     }
     return (
       <>
-      <button onClick={redirectUser}>Redirect Button</button>
+        <button onClick={redirectUser}>Redirect Button</button>
         <div>Welcome to the allen home page</div>
       </>
 
-)
-}
+    )
+  }
 
-function Class11() {
-  return (
+  function Class11() {
+    return (
       <>
         <div>This is class 11th Program</div>
       </>
@@ -52,8 +83,7 @@ function Class11() {
       </>
     )
   }
-  
-  
+
+
 }
-  export default App
-  
+export default App
