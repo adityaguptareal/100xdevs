8f1a1a1c6c69fcebd0e4bd59b88ccac032b7652d
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-12 22:44:12 +0530

    start stop timer using useRef hook

--- week-10/src/App.jsx
+++ week-10/src/App.jsx
@@ -1,89 +1,27 @@
-import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
-import './App.css'
+import { useRef, useState } from "react"
 
-function App() {
+function App(params) {
 
+  const [time, setTime] = useState(0)
+  const timeRef = useRef()
+  function startTimer() {
+    let clock = setInterval(() => {
+      setTime(c => c + 1)
+    }, 1000);
+    timeRef.current = clock
+
+  }
+
+  function stopTimer() {
+    clearInterval(timeRef.current)
+  }
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />} >
-            <Route index element={<Allen />} />
-            <Route path='/Class11' element={<Class11 />} />
-            <Route path='/Class12' element={<Class12 />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
-
+      <div>{time}</div>
+      <br />
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
     </>
-
   )
-
-  function Layout() {
-    return (
-      <>
-        <div>
-
-          <Header />
-        </div>
-        <div>
-          <Outlet />
-        </div>
-        <div>
-
-          <Footer />
-        </div>
-      </>
-
-    )
-  }
-  function Footer() {
-    return (
-      <div style={{ width: "100vw", backgroundColor: "red" }}>This is footer</div>
-    )
-  }
-  function Header() {
-    return (
-
-      <div style={{ width: "100vw", backgroundColor: 'blue', color: "white" }}>
-        <Link to="/">Allen |</Link>
-        <Link to="/Class11">Class 11 |</Link>
-        <Link to="/Class12">Class 12 |</Link>
-
-      </div>
-    )
-  }
-
-  function Allen() {
-    const naigate = useNavigate()
-    function redirectUser() {
-      naigate("/Clsss12")
-    }
-    return (
-      <>
-        <button onClick={redirectUser}>Redirect Button</button>
-        <div>Welcome to the allen home page</div>
-      </>
-
-    )
-  }
-
-  function Class11() {
-    return (
-      <>
-        <div>This is class 11th Program</div>
-      </>
-    )
-  }
-  function Class12() {
-    return (
-      <>
-        <div>This is class 12th Program</div>
-      </>
-    )
-  }
-
-
 }
 export default App
