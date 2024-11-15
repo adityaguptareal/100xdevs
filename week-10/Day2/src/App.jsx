aa230b70dbac17860d9d44705e1ca202429c53ac
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-16 00:04:47 +0530

    State rolling up in react

--- week-10/Day2/src/App.jsx
+++ week-10/Day2/src/App.jsx
@@ -4,13 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    hi there
+      <LightBulb></LightBulb>
     </>
   )
 }
 
+function LightBulb() {
+  const [bulb, setbulb] = useState(false)
+  return (
+    <>
+      <BulbState bulb={bulb}></BulbState>
+      <ToggleButton setbulb={setbulb} bulb={bulb}></ToggleButton>
+    </>
+  )
+}
+
+function BulbState({bulb}) {
+  return (
+    <>
+      {bulb ? "Bulb is on" : "Bulb is off"}
+    </>
+  )
+}
+
+function ToggleButton({setbulb}) {
+  return (
+    <>
+    <br />
+     <button onClick={()=>setbulb(currentState=>!currentState)}>Toggle Button</button>
+    </>
+  )
+}
 export default App
