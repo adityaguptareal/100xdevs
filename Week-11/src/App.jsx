3fc8d669765b22344ca331eb04d5c1eafd06baeb
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-17 18:41:59 +0530

    useFetch hook with auto fetch feature

--- Week-11/src/App.jsx
+++ Week-11/src/App.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { useState } from "react";
+import { useFetch } from "./hooks/UseFetch";
 
-function useCounter() {
-  const [count, setCount] = useState(0)
-  function increaseCount() {
-    setCount(c => c + 1)
-  }
-  return {
-    count: count,
-    increaseCount: increaseCount
-  }
-}
+
+// function useCounter() {
+//   const [count, setCount] = useState(0)
+//   function increaseCount() {
+//     setCount(c => c + 1)
+//   }
+//   return {
+//     count: count,
+//     increaseCount: increaseCount
+//   }
+// }
 
 function App() {
-  const{count,increaseCount}=useCounter()
+  const [currentPost, setCurrentPost] = useState(1)
+  const { post, loading } = useFetch('https://jsonplaceholder.typicode.com/todos/' + currentPost,5)
+  // const{count,increaseCount}=useCounter()
   return (
     <>
-      <button onClick={increaseCount}>Counter {count}</button>
+      {/* <button onClick={increaseCount}>Counter {count}</button> */}
+      <button onClick={() => setCurrentPost(1)}>Post 1</button>
+      <button onClick={() => setCurrentPost(2)}>Post 2</button>
+      <button onClick={() => setCurrentPost(3)}>Post 3</button>
+
+      {loading ? <div>Loading...</div> : <div>{post.title}</div>}
+
     </>
   )
 }
