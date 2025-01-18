e9e0fa187f5f97436dfa6799abeb2af7f3e487f8
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-01-18 23:52:16 +0530

    Recap test

---
+++ week-11/week-11-Recoil/recap/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { value } from './AtomStore'
+import {RecoilRoot, useRecoilValue, useSetRecoilState} from "recoil"
+
+function App() {
+
+  return (<>
+    <RecoilRoot>
+    <Counter/>
+    </RecoilRoot>
+  </>
+  )
+}
+
+function Counter() {
+  return(
+    <>
+    <CurrentCount/>
+    <IncreaseCounter/>
+    <DecreaseCounter/>
+    </>
+  )
+}
+
+function CurrentCount() {
+  const count=useRecoilValue(value)
+  return(
+    <div>Current Count: {count}</div>
+  )
+}
+function IncreaseCounter(){
+  const setValue=useSetRecoilState(value)
+  return(
+    <button onClick={()=>setValue((c)=>c+1)}>Increase Count </button>
+  )
+}
+
+function DecreaseCounter(){
+  const setValue=useSetRecoilState(value)
+  return(
+    <button onClick={()=>setValue((c)=>c-1)}>Decrease Count </button>
+  )
+}
+
+export default App
---
+++ week-11/week-11-Recoil/recap/src/AtomStore.jsx
@@ -0,0 +1,5 @@
+import { atom } from "recoil";
+export const value=atom({
+    key:"value",
+    default:0,
+})
